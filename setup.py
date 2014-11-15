f058a5d4134684bf97a827b6c5d3917949cdad6f
TakesxiSximada <takesxi.sximada@gmail.com>
2014-11-15 20:19:03 +0900

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,29 @@
-#-*- coding: utf-8 -*-
-from setuptools import setup, find_packages
-import sys, os
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from setuptools import (
+    setup,
+    find_packages,
+    )
 
-version = '0.1.2'
+
+def find_package_data(target, package_root):
+    return [
+        os.path.relpath(os.path.join(root, filename), package_root)
+        for root, dirs, files in os.walk(target)
+        for filename in files
+        ]
+
+src = 'src'
+requires = [
+    'six',
+    ]
+install_requires = []
+test_require = []
+packages = find_packages(src)
+package_dir = {'': src}
+package_data = {}
+
 long_description = 'This allows to use the CSV as ORM'
 try:
     with open('README') as fp:
@@ -10,30 +31,40 @@ try:
 except:
     pass
 
-setup(name='csvorm',
-      version=version,
-      description="orm for csv",
-      long_description=long_description,
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'License :: OSI Approved :: BSD License',
-          'Natural Language :: English',
-          'Natural Language :: Japanese',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.7',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='csv',
-      author='TakesxiSximada',
-      url='https://bitbucket.org/takesxi_sximada/csvorm',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+
+setup(
+    name='csvorm',
+    version='0.1.2',
+    url='https://github.com/TakesxiSximada/csvorm',
+    download_url='https://github.com/TakesxiSximada/csvorm',
+    license='See http://www.python.org/3.4/license.html',
+    author='TakesxiSximada',
+    author_email='takesxi.sximada@gmail.com',
+    description='orm for csv',
+    long_description=long_description,
+    keywords='csv',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        ],
+    platforms='any',
+    packages=packages,
+    package_dir=package_dir,
+    package_data=package_data,
+    include_package_data=True,
+    requires=requires,
+    install_requires=install_requires,
+    test_require=test_require,
+    entry_points='''
+    [console_scripts]
+    '''
+    )
